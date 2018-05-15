00560235773995608032019f8955d958ab8d21b4
Javi <fj.gutierrezs91@gmail.com>
2018-05-15 22:34:03 +0200

    Combinate channels working...

--- 3/color-filter-cuda/func.cu
+++ 3/color-filter-cuda/func.cu
@@ -11,6 +11,9 @@
 // checkCudaErrors(cudaMalloc(&d_red, sizeof(unsigned char) * numRows * numCols));
 //****************************************************************************
 
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #include <iostream>
 #include <iomanip>
 #include <algorithm>    // std::max
@@ -19,51 +22,56 @@
 #include <cuda_runtime_api.h>
 
 typedef unsigned char uchar;
+
 #define FILTER_WIDTH 3
+#define NTHREADS 32
+
 #define checkCudaErrors(val) check( (val), #val, __FILE__, __LINE__)
 
 template<typename T>
 void check(T err, const char* const func, const char* const file, const int line) {
-  if (err != cudaSuccess) {
-    std::cerr << "CUDA error at: " << file << ":" << line << std::endl;
-    std::cerr << cudaGetErrorString(err) << " " << func << std::endl;
-    exit(1);
-  }
+      if (err != cudaSuccess) {
+            std::cerr << "CUDA error at: " << file << ":" << line << std::endl;
+            std::cerr << cudaGetErrorString(err) << " " << func << std::endl;
+            exit(1);
+      }
 }
 
-__constant__ float filtro[FILTER_WIDTH*FILTER_WIDTH];
+__constant__ float filtro[FILTER_WIDTH * FILTER_WIDTH];
 
-__global__
-void box_filter(const unsigned char* const inputChannel,
-                   unsigned char* const outputChannel,
-                   int numRows, int numCols,
-                   const float* __restrict__ filter, const int filterWidth)
+__global__ void box_filter(const unsigned char* const inputChannel,
+                        unsigned char* const outputChannel,
+                        int numRows, int numCols,
+                        const float* __restrict__ filter, const int filterWidth)
 {
-	//Aplicar el filtro a cada pixel de la imagen
+      // Aplicar el filtro a cada pixel de la imagen
 
-  // NOTA: Que un thread tenga una posici�n correcta en 2D no quiere decir que al aplicar el filtro
-  // los valores de sus vecinos sean correctos, ya que pueden salirse de la imagen.
+      // NOTA: Que un thread tenga una posición correcta en 2D no quiere decir que al aplicar el filtro
+      // los valores de sus vecinos sean correctos, ya que pueden salirse de la imagen.
 }
 
-
-
-//This kernel takes in an image represented as a uchar4 and splits
-//it into three images consisting of only one color channel each
-__global__
-void separateChannels(const uchar4* const inputImageRGBA,
+// This kernel takes in an image represented as a uchar4 and splits
+// it into three images consisting of only one color channel each
+__global__ void separateChannels(const uchar4* const inputImageRGBA,
                       int numRows,
                       int numCols,
                       unsigned char* const redChannel,
                       unsigned char* const greenChannel,
                       unsigned char* const blueChannel)
 {
+        int idx = blockIdx.x * blockDim.x + threadIdx.x;
+        int idy = blockIdx.y * blockDim.y + threadIdx.y;
+        if (idx >= numCols || idy >= numRows) return;
 
-	//Separar la imagen en sus canales RGB
-}
+        int id = idy * numCols + idx;
+        redChannel[id] = inputImageRGBA[id].x;
+        greenChannel[id] = inputImageRGBA[id].y;
+        blueChannel[id] = inputImageRGBA[id].z;
+ }
 
-//This kernel takes in three color channels and recombines them
-//into one image. The alpha channel is set to 255 to represent
-//that this image has no transparency.
+// This kernel takes in three color channels and recombines them
+// into one image. The alpha channel is set to 255 to represent
+// that this image has no transparency.
 __global__
 void recombineChannels(const unsigned char* const redChannel,
                        const unsigned char* const greenChannel,
@@ -72,44 +80,91 @@ void recombineChannels(const unsigned char* const redChannel,
                        int numRows,
                        int numCols)
 {
-	//Recombinar los canales RGB en una sola imagen
+      int idx = blockIdx.x * blockDim.x + threadIdx.x;
+      int idy = blockIdx.y * blockDim.y + threadIdx.y;
+
+      //make sure we don't try and access memory outside the image
+      //by having any threads mapped there return early
+      if (idx >= numCols || idy >= numRows) return;
+      int id = idy * numCols + idx;
+
+      unsigned char red   = redChannel[id];
+      unsigned char green = greenChannel[id];
+      unsigned char blue  = blueChannel[id];
+
+      // Alpha should be 255 for no transparency
+      uchar4 outputPixel = make_uchar4(red, green, blue, 255);
+      outputImageRGBA[id] = outputPixel;
 }
 
 unsigned char *d_red, *d_green, *d_blue;
-float         *d_filter;
+float *d_filter;
 
 void allocateMemoryAndCopyToGPU(const size_t numRowsImage, const size_t numColsImage,
                                 const float* const h_filter, const size_t filterWidth)
 {
-  //Reservar memoria para los 3 canales y el filtro de color
+      int sizeImg = sizeof(uchar4) * numRowsImage * numColsImage;
+      int sizeFilter = sizeof(uchar4) * filterWidth * filterWidth;
+
+      cudaMalloc(&d_red, sizeImg);
+      cudaMalloc(&d_green, sizeImg);
+      cudaMalloc(&d_blue, sizeImg);
+      cudaMalloc(&d_filter, sizeFilter);
+
+      cudaMemcpy(d_filter, h_filter, sizeFilter, cudaMemcpyHostToDevice);
+      cudaMemset(d_red, 0, sizeImg);
+      cudaMemset(d_green, 0, sizeImg);
+      cudaMemset(d_blue, 0, sizeImg);
 }
 
-
-void create_filter(float **h_filter, int *filterWidth){
-
-	//crear el filtro se que va a aplicar (en CPU) y almacenar su tamaño
+// Crear el filtro se que va a aplicar (en CPU) y almacenar su tamaño...
+void create_filter(float **h_filter, int *filterWidth)
+{
+      float *filter = (float *) malloc(sizeof(float) * FILTER_WIDTH * FILTER_WIDTH);
+      if (!h_filter) {
+            std::cerr << "Error creating filter.." << strerror(errno) << '\n';
+            exit(1);
+      }
+      float mask[] = {
+            1, 0, 1,
+            0, 1, 0,
+            1, 0, 1
+      };
+      for (int i = 0; i < FILTER_WIDTH * FILTER_WIDTH; ++i) {
+            filter[i] = mask[i];
+      }
+      *h_filter = filter;
+      *filterWidth = FILTER_WIDTH;
 }
 
-
-void convolution(const uchar4 * const h_inputImageRGBA, uchar4 * const d_inputImageRGBA,
+void convolution(const uchar4* const h_inputImageRGBA, uchar4* const d_inputImageRGBA,
                         uchar4* const d_outputImageRGBA, const size_t numRows, const size_t numCols,
-                        unsigned char *d_redFiltered, 
-                        unsigned char *d_greenFiltered, 
+                        unsigned char *d_redFiltered,
+                        unsigned char *d_greenFiltered,
                         unsigned char *d_blueFiltered,
                         const int filterWidth)
 {
-	//Separar la imagen en sus canales rgb
-	//aplicar el filtro a cada canal
-	//volver a juntar la imagen
+      const dim3 blockSize(NTHREADS, NTHREADS, 1);
+      const dim3 gridSize((numCols - 1) / blockSize.x + 1, (numRows - 1) / blockSize.y + 1, 1);
 
+      fprintf(stderr, "Num blocks : (x:%d, y:%d, z:%d)\n", gridSize.x, gridSize.y);
+      separateChannels<<<gridSize, blockSize>>>(d_inputImageRGBA, numRows, numCols, d_redFiltered, d_greenFiltered, d_blueFiltered);
+
+      recombineChannels<<<gridSize, blockSize>>>(d_redFiltered, d_greenFiltered, d_blueFiltered, d_outputImageRGBA,
+                                                   numRows,
+                                                   numCols);
+
+	// Separar la imagen en sus canales rgb (separateChannels)
+	// aplicar el filtro a cada canal (create_filter) (box_filter)
+	// volver a juntar la imagen (recombineChannels)...
 }
 
 
 //Free all the memory that we allocated
-//TODO: make sure you free any arrays that you allocated
+// TODO: make sure you free any arrays that you allocated
 void cleanup() {
-  checkCudaErrors(cudaFree(d_red));
-  checkCudaErrors(cudaFree(d_green));
-  checkCudaErrors(cudaFree(d_blue));
-  checkCudaErrors(cudaFree(d_filter));
+      checkCudaErrors(cudaFree(d_red));
+      checkCudaErrors(cudaFree(d_green));
+      checkCudaErrors(cudaFree(d_blue));
+      checkCudaErrors(cudaFree(d_filter));
 }
--- 3/color-filter-cuda/main.cpp
+++ 3/color-filter-cuda/main.cpp
@@ -1,6 +1,7 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <string>
+#include <errno.h>
 #include <stdio.h>
 #include <iostream>
 #include <iomanip>
@@ -23,16 +24,14 @@ size_t numCols() { return imageInputRGBA.cols; }
 /*******  DEFINED IN func.cu *********/
 void create_filter(float **h_filter, int *filterWidth);
 
-void convolution(const uchar4 * const h_inputImageRGBA, uchar4 * const d_inputImageRGBA,
-                        uchar4* const d_outputImageRGBA,
+void convolution(const uchar4 * const h_inputImageRGBA, uchar4 * const d_inputImageRGBA, uchar4* const d_outputImageRGBA,
                         const size_t numRows, const size_t numCols,
                         unsigned char *d_redFiltered,
                         unsigned char *d_greenFiltered,
                         unsigned char *d_blueFiltered,
                         const int filterWidth);
 
-void allocateMemoryAndCopyToGPU(const size_t numRowsImage, const size_t numColsImage,
-                                const float* const h_filter, const size_t filterWidth);
+void allocateMemoryAndCopyToGPU(const size_t numRowsImage, const size_t numColsImage, const float* const h_filter, const size_t filterWidth);
 
 //****************************************************************************
 // Also note that we've supplied a helpful debugging function called checkCudaErrors.
@@ -51,164 +50,195 @@ void allocateMemoryAndCopyToGPU(const size_t numRowsImage, const size_t numColsI
 
 template<typename T>
 void check(T err, const char* const func, const char* const file, const int line) {
-  if (err != cudaSuccess) {
-    std::cerr << "CUDA error at: " << file << ":" << line << std::endl;
-    std::cerr << cudaGetErrorString(err) << " " << func << std::endl;
-    exit(1);
-  }
+      if (err != cudaSuccess) {
+            std::cerr << "CUDA error at: " << file << ":" << line << std::endl;
+            std::cerr << cudaGetErrorString(err) << " " << func << std::endl;
+            exit(1);
+      }
 }
 
-//return types are void since any internal error will be handled by quitting
-//no point in returning error codes...
-//returns a pointer to an RGBA version of the input image
-//and a pointer to the single channel grey-scale output
-//on both the host and device
+// Return types are void since any internal error will be handled by quitting
+// no point in returning error codes...
+// returns a pointer to an RGBA version of the input image
+// and a pointer to the single channel grey-scale output
+// on both the host and device.
 void preProcess(uchar4 **h_inputImageRGBA, uchar4 **h_outputImageRGBA,
                 uchar4 **d_inputImageRGBA, uchar4 **d_outputImageRGBA,
                 unsigned char **d_redFiltered,
                 unsigned char **d_greenFiltered,
                 unsigned char **d_blueFiltered,
                 float **h_filter, int *filterWidth,
-                const std::string &filename) {
+                const std::string &filename)
+{
 
-  //make sure the context initializes ok
-  checkCudaErrors(cudaFree(0));
+      //make sure the context initializes ok
+      checkCudaErrors(cudaFree(0));
 
-  cv::Mat image = cv::imread(filename.c_str(), CV_LOAD_IMAGE_COLOR);
-  if (image.empty()) {
-    std::cerr << "Couldn't open file: " << filename << std::endl;
-    exit(1);
-  }
+      cv::Mat image = cv::imread(filename.c_str(), CV_LOAD_IMAGE_COLOR);
+      if (image.empty()) {
+            std::cerr << "Couldn't open file: " << filename << std::endl;
+            exit(1);
+      }
+      cv::cvtColor(image, imageInputRGBA, CV_BGR2RGBA);
 
-  cv::cvtColor(image, imageInputRGBA, CV_BGR2RGBA);
+      //allocate memory for the output
+      imageOutputRGBA.create(image.rows, image.cols, CV_8UC4);
 
-  //allocate memory for the output
-  imageOutputRGBA.create(image.rows, image.cols, CV_8UC4);
+      // This shouldn't ever happen given the way the images are created
+      // at least based upon my limited understanding of OpenCV, but better to check...
+      if (!imageInputRGBA.isContinuous() || !imageOutputRGBA.isContinuous()) {
+            std::cerr << "Images aren't continuous!! Exiting." << std::endl;
+            exit(1);
+      }
 
-  //This shouldn't ever happen given the way the images are created
-  //at least based upon my limited understanding of OpenCV, but better to check
-  if (!imageInputRGBA.isContinuous() || !imageOutputRGBA.isContinuous()) {
-    std::cerr << "Images aren't continuous!! Exiting." << std::endl;
-    exit(1);
-  }
+      *h_inputImageRGBA  = (uchar4 *) imageInputRGBA.ptr<unsigned char>(0);
+      *h_outputImageRGBA = (uchar4 *) imageOutputRGBA.ptr<unsigned char>(0);
 
-  *h_inputImageRGBA  = (uchar4 *)imageInputRGBA.ptr<unsigned char>(0);
-  *h_outputImageRGBA = (uchar4 *)imageOutputRGBA.ptr<unsigned char>(0);
+      const size_t numPixels = numRows() * numCols();
+      // Allocate memory on the device for both input and output...
+      checkCudaErrors(cudaMalloc(d_inputImageRGBA, sizeof(uchar4) * numPixels));
+      checkCudaErrors(cudaMalloc(d_outputImageRGBA, sizeof(uchar4) * numPixels));
+      checkCudaErrors(cudaMemset(*d_outputImageRGBA, 0, numPixels * sizeof(uchar4))); //make sure no memory is left laying around
 
-  const size_t numPixels = numRows() * numCols();
-  //allocate memory on the device for both input and output
-  checkCudaErrors(cudaMalloc(d_inputImageRGBA, sizeof(uchar4) * numPixels));
-  checkCudaErrors(cudaMalloc(d_outputImageRGBA, sizeof(uchar4) * numPixels));
-  checkCudaErrors(cudaMemset(*d_outputImageRGBA, 0, numPixels * sizeof(uchar4))); //make sure no memory is left laying around
+      // copy input array to the GPU...
+      checkCudaErrors(cudaMemcpy(*d_inputImageRGBA, *h_inputImageRGBA, sizeof(uchar4) * numPixels, cudaMemcpyHostToDevice));
 
-  //copy input array to the GPU
-  checkCudaErrors(cudaMemcpy(*d_inputImageRGBA, *h_inputImageRGBA, sizeof(uchar4) * numPixels, cudaMemcpyHostToDevice));
+      d_inputImageRGBA__  = *d_inputImageRGBA;
+      d_outputImageRGBA__ = *d_outputImageRGBA;
 
-  d_inputImageRGBA__  = *d_inputImageRGBA;
-  d_outputImageRGBA__ = *d_outputImageRGBA;
+      create_filter(h_filter, filterWidth);
 
-  create_filter(h_filter, filterWidth);
+      h_filter__ = *h_filter;
 
-  h_filter__ = *h_filter;
-
-  checkCudaErrors(cudaMalloc(d_redFiltered,    sizeof(unsigned char) * numPixels));
-  checkCudaErrors(cudaMalloc(d_greenFiltered,  sizeof(unsigned char) * numPixels));
-  checkCudaErrors(cudaMalloc(d_blueFiltered,   sizeof(unsigned char) * numPixels));
-  checkCudaErrors(cudaMemset(*d_redFiltered,   0, sizeof(unsigned char) * numPixels));
-  checkCudaErrors(cudaMemset(*d_greenFiltered, 0, sizeof(unsigned char) * numPixels));
-  checkCudaErrors(cudaMemset(*d_blueFiltered,  0, sizeof(unsigned char) * numPixels));
+      checkCudaErrors(cudaMalloc(d_redFiltered,    sizeof(unsigned char) * numPixels));
+      checkCudaErrors(cudaMalloc(d_greenFiltered,  sizeof(unsigned char) * numPixels));
+      checkCudaErrors(cudaMalloc(d_blueFiltered,   sizeof(unsigned char) * numPixels));
+      checkCudaErrors(cudaMemset(*d_redFiltered,   0, sizeof(unsigned char) * numPixels));
+      checkCudaErrors(cudaMemset(*d_greenFiltered, 0, sizeof(unsigned char) * numPixels));
+      checkCudaErrors(cudaMemset(*d_blueFiltered,  0, sizeof(unsigned char) * numPixels));
 }
 
 void postProcess(const std::string& output_file, uchar4* data_ptr) {
-  cv::Mat output(numRows(), numCols(), CV_8UC4, (void*)data_ptr);
+      cv::Mat output(numRows(), numCols(), CV_8UC4, (void*) data_ptr);
 
-  cv::Mat imageOutputBGR;
-  cv::cvtColor(output, imageOutputBGR, CV_RGBA2BGR);
-  //output the image
-  cv::imwrite(output_file.c_str(), imageOutputBGR);
+      cv::Mat imageOutputBGR;
+      cv::cvtColor(output, imageOutputBGR, CV_RGBA2BGR);
+
+      // Output the image...
+      cv::imwrite(output_file.c_str(), imageOutputBGR);
 }
 
 void cleanUp(void)
 {
-  cudaFree(d_inputImageRGBA__);
-  cudaFree(d_outputImageRGBA__);
-  delete[] h_filter__;
+      cudaFree(d_inputImageRGBA__);
+      cudaFree(d_outputImageRGBA__);
+      delete[] h_filter__;
 }
 
 
-// An unused bit of code showing how to accomplish this assignment using OpenCV.  It is much faster 
+// An unused bit of code showing how to accomplish this assignment using OpenCV.  It is much faster
 //    than the naive implementation in reference_calc.cpp.
 void generateReferenceImage(std::string input_file, std::string reference_file, int kernel_size)
 {
 	cv::Mat input = cv::imread(input_file);
-	// Create an identical image for the output as a placeholder
+
+	// Create an identical image for the output as a placeholder.
 	cv::Mat reference = cv::imread(input_file);
 	cv::GaussianBlur(input, reference, cv::Size2i(kernel_size, kernel_size),0);
 	cv::imwrite(reference_file, reference);
 }
 
+void debug_image(char *filename, unsigned char *pixels, int rows, int cols)
+{
+      cv::Mat output(numRows(), numCols(), CV_8UC1, (void*) pixels);
+
+//      cv::Mat imageOutputBGR;
+//      cv::cvtColor(output, imageOutputBGR, CV_RGBA2BG);
+
+      // Output the image...
+      cv::imwrite(filename, output);
+}
+
+void debug_devices_data(char *filename, unsigned char *d_data, int rows, int cols)
+{
+      int size = sizeof(unsigned char) * rows * cols;
+      unsigned char *h_data = (unsigned char*) malloc(size);
+      if (!h_data) {
+            std::cerr << "Error allocating memory to debug image..." << strerror(errno) << '\n';
+            exit(1);
+      }
+      cudaMemcpy(h_data, d_data, size, cudaMemcpyDeviceToHost);
+      debug_image(filename, h_data, rows, cols);
+      free(h_data);
+}
+
 /*******  Begin main *********/
-
 int main(int argc, char **argv) {
-  uchar4 *h_inputImageRGBA,  *d_inputImageRGBA;
-  uchar4 *h_outputImageRGBA, *d_outputImageRGBA;
-  unsigned char *d_redFiltered, *d_greenFiltered, *d_blueFiltered;
+      uchar4 *h_inputImageRGBA, *d_inputImageRGBA;
+      uchar4 *h_outputImageRGBA, *d_outputImageRGBA;
+      unsigned char *d_redFiltered, *d_greenFiltered, *d_blueFiltered;
 
-  float *h_filter;
-  int    filterWidth;
+      float *h_filter;
+      int filterWidth;
 
-  std::string input_file;
-  std::string output_file;
-  std::string reference_file;
-  bool useEpsCheck = false;
-  switch (argc)
-  {
-	case 2:
-	  input_file = std::string(argv[1]);
-	  output_file = "output.png";
-	  break;
-	case 3:
-	  input_file  = std::string(argv[1]);
-      output_file = std::string(argv[2]);
-	  break;
-	default:
-      std::cerr << "Usage: ./box_filter input_file [output_filename]" << std::endl;
-      exit(1);
-  }
-  //load the image and give us our input and output pointers
-  preProcess(&h_inputImageRGBA, &h_outputImageRGBA, &d_inputImageRGBA, &d_outputImageRGBA,
-             &d_redFiltered, &d_greenFiltered, &d_blueFiltered,
-             &h_filter, &filterWidth, input_file);
+      std::string input_file;
+      std::string output_file;
+      std::string reference_file;
+      bool useEpsCheck = false;
+      switch (argc)
+      {
+            case 2:
+            input_file = std::string(argv[1]);
+            output_file = "output.png";
+            break;
 
-  allocateMemoryAndCopyToGPU(numRows(), numCols(), h_filter, filterWidth);
-  GpuTimer timer;
-  timer.Start();
-  //call the students' code
-  convolution(h_inputImageRGBA, d_inputImageRGBA, d_outputImageRGBA, numRows(), numCols(),
-                     d_redFiltered, d_greenFiltered, d_blueFiltered, filterWidth);
-  timer.Stop();
-  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
-  int err = printf("Your code ran in: %f msecs.\n", timer.Elapsed());
+            case 3:
+            input_file  = std::string(argv[1]);
+            output_file = std::string(argv[2]);
+            break;
 
-  if (err < 0) {
-    //Couldn't print! Probably the student closed stdout - bad news
-    std::cerr << "Couldn't print timing information! STDOUT Closed!" << std::endl;
-    exit(1);
-  }
+            default:
+            std::cerr << "Usage: ./box_filter input_file [output_filename]" << std::endl;
+            exit(1);
+      }
+      // Load the image and give us our input and output pointers
+      preProcess(&h_inputImageRGBA, &h_outputImageRGBA, &d_inputImageRGBA, &d_outputImageRGBA,
+                  &d_redFiltered, &d_greenFiltered, &d_blueFiltered,
+                  &h_filter, &filterWidth, input_file);
 
-  //write results 
+      allocateMemoryAndCopyToGPU(numRows(), numCols(), h_filter, filterWidth);
 
-  size_t numPixels = numRows()*numCols();
-  //copy the output back to the host
-  checkCudaErrors(cudaMemcpy(h_outputImageRGBA, d_outputImageRGBA__, sizeof(uchar4) * numPixels, cudaMemcpyDeviceToHost));
+      GpuTimer timer;
+      timer.Start();
 
-  postProcess(output_file, h_outputImageRGBA);
+      // call the students' code...
+      convolution(h_inputImageRGBA, d_inputImageRGBA, d_outputImageRGBA, numRows(), numCols(), d_redFiltered, d_greenFiltered, d_blueFiltered,
+                  filterWidth);
 
-  checkCudaErrors(cudaFree(d_redFiltered));
-  checkCudaErrors(cudaFree(d_greenFiltered));
-  checkCudaErrors(cudaFree(d_blueFiltered));
+      debug_devices_data("red.png", d_redFiltered, numRows(), numCols());
 
-  cleanUp();
+      timer.Stop();
 
-  return 0;
+      cudaDeviceSynchronize();
+      checkCudaErrors(cudaGetLastError());
+
+      int err = printf("Your code ran in: %f msecs.\n", timer.Elapsed());
+      if (err < 0) {
+            // Couldn't print! Probably the student closed stdout - bad news...
+            std::cerr << "Couldn't print timing information! STDOUT Closed!" << std::endl;
+            exit(1);
+      }
+
+      // Write results...
+      size_t numPixels = numRows() * numCols();
+
+      // Copy the output back to the host...
+      checkCudaErrors(cudaMemcpy(h_outputImageRGBA, d_outputImageRGBA__, sizeof(uchar4) * numPixels, cudaMemcpyDeviceToHost));
+      postProcess(output_file, h_outputImageRGBA);
+
+      checkCudaErrors(cudaFree(d_redFiltered));
+      checkCudaErrors(cudaFree(d_greenFiltered));
+      checkCudaErrors(cudaFree(d_blueFiltered));
+      cleanUp();
+      return 0;
 }
